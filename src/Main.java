ab8dc99ae10421df7c2d3722864104813ad7bb48
Maria Denisova <mvdenisova.work@gmail.com>
2023-09-20 23:34:02 +0300

    completed tast (1 version)

--- src/Main.java
+++ src/Main.java
@@ -23,11 +23,32 @@ public class Main {
             m = in.nextInt();
         }
         int[][] matrix = new int[n][m];
+        int[] minimum = new int[(n - 1) * (m - 1)];
+        int c = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 matrix[i][j] = in.nextInt();
             }
         }
+
+        for (int i = 1; i < n - 1; i++) {
+            for (int j = 1; j < m - 1; j++) {
+                if (matrix[i][j] < matrix[i - 1][j] && matrix[i][j] < matrix[i + 1][j] && matrix[i][j] < matrix[i][j + 1] && matrix[i][j] < matrix[i][j - 1]) {
+                    minimum[c] = matrix[i][j];
+                    c++;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < c; i++) {
+            if (minimum[i] > max)
+                max = minimum[i];
+        }
+        if (max == 0)
+            System.out.println("No local minimum in matrix");
+        else
+            System.out.println("Maximum of local minimum is " + max);
     }
 }
